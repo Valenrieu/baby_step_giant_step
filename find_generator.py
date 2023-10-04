7b74112e85264edb1643c4917a605654e2188207
Valenrieu <119423840+Valenrieu@users.noreply.github.com>
2023-10-04 08:31:07 +0200

    Update find_generator.py

--- find_generator.py
+++ find_generator.py
@@ -1,21 +1,22 @@
-#permet de trouver les generateurs de Z/nZ
 def isgenerator(n, g):
-    nombres = set()
+    """Check if g is a generator of (Z/nZ, X)^X"""
+    numbers = set()
 
     for i in range(n):
-        nombres.add(pow(g, i, n))
+        numbers.add(pow(g, i, n))
 
-    if len(nombres)+1==n:
+    if len(numbers)+1==n:
         return True
     
     return False
 
 def findgenerator(n):
+    """Find all generators of (Z/nZ, X)^X"""
     for i in range(n):
-        nombres = set()
+        numbers = set()
 
         for j in range(n):
-            nombres.add(pow(i, j, n))
+            numbers.add(pow(i, j, n))
 
-        if len(nombres)+1==n:
+        if len(numbers)+1==n:
             print(i)
