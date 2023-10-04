1f46966d4e606a7900612e36debc8754adfecf24
Valenrieu <119423840+Valenrieu@users.noreply.github.com>
2023-10-04 08:05:12 +0200

    Add files via upload

---
+++ find_generator.py
@@ -0,0 +1,22 @@
+#permet de trouver les generateurs de Z/nZ
+def isgenerator(n, g):
+    nombres = set()
+
+    for i in range(n):
+        nombres.add(pow(g, i, n))
+
+    if len(nombres)+1==n:
+        return True
+    
+    return False
+
+def findgenerator(n):
+
+    for i in range(n):
+        nombres = set()
+
+        for j in range(n):
+            nombres.add(pow(i, j, n))
+
+        if len(nombres)+1==n:
+            print(i)
